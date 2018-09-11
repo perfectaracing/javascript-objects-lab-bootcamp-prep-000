0e2e36a795e86f4e985688efad9a5dec0c843867
Loree Nichols <loree.d.nichols@gmail.com>
2018-09-11 02:04:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,3 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
-
-
-function updateObjectWithKeyAndValue(object, key, value) {
- var newObj = Object.assign({}, key);
- console.log(newObj);
- console.log(recipes);
-}
